c1b774dbd4185f9d551b44fd9185cf79f35cca30
Nguyễn Hoài Bảo <22110108@student.hcmute.edu.vn>
2026-02-06 21:36:46 +0700

    check friendship middleware

---
+++ backend/src/middlewares/friendMiddleware.js
@@ -0,0 +1,24 @@
+import Friend from '../models/friendModel.js';
+
+const pair = (a, b) => (a < b ? [a, b] : [b, a]);
+
+export async function checkFriendship(req, res, next) {
+    try {
+        const senderId = req.user._id.toString();
+        const recipientId = req.body?.recipientId ?? null;
+        if (!recipientId) {
+            return res.status(400).json({ message: 'Recipient ID is required.' });
+        }
+        if (recipientId) {
+            const [userA, userB] = pair(senderId, recipientId);
+            const friendship = await Friend.findOne({ userA, userB });
+            if (!friendship) {
+                return res.status(403).json({ message: 'You are not friends with this user.' });
+            }
+            return next();
+        }
+    } catch (error) {
+        console.error('Error checking friendship:', error);
+        return res.status(500).json({ message: 'Internal server error.' });
+    }
+}
--- backend/src/routes/messageRoute.js
+++ backend/src/routes/messageRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { sendDirectMessage } from '../controllers/messageController.js';
+import { checkFriendship } from '../middlewares/friendMiddleware.js';
 
 const messageRouter = express.Router();
 
-messageRouter.post('/send-direct', sendDirectMessage);
+messageRouter.post('/send-direct', checkFriendship, sendDirectMessage);
 
 export default messageRouter;
