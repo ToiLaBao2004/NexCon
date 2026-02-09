9ba01151c9f989bc9fdacb11dd874450bc6f096c
Tnthien204 <trannhuthien2004@gmail.com>
2026-02-09 21:25:15 +0700

    feat: update backend/src/server.js, add backend/src/socketHandle.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,9 +9,13 @@ import userRouter from './routes/userRoute.js';
 import otpRouter from './routes/otpRoute.js';
 import friendRouter from './routes/friendRoute.js';
 import messageRouter from './routes/messageRoute.js';
+import { Server } from "socket.io";
+import { SocketServer } from "./socketHandle.js";
 
-const app = express();
+
+const app = express();    
 const PORT = process.env.PORT;
+
 
 // middlewares
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
@@ -29,8 +33,29 @@ app.use('/api/users', userRouter);
 app.use('/api/friends', friendRouter);
 app.use('/api/messages', messageRouter);
 
-connectDB().then(() => {
+/* connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on: http://localhost:${PORT}`);
     })
+}); */
+
+let server;
+
+connectDB().then(() => {
+  server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+
+  const io = new Server(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: process.env.CLIENT_URL,
+      credentials: true
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log('Socket io connected Successfully!":', socket.id);
+    SocketServer(socket, io);
+  });
 });
