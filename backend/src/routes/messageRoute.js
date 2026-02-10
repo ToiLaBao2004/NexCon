566cf3701e316d09c7f9d386112b5048158bec18
Tnthien204 <trannhuthien2004@gmail.com>
2026-02-10 16:11:49 +0700

    Add group messaging endpoint and middleware - backend/src/controllers/messageController.js: add sendGroupMessage - backend/src/middlewares/groupMiddleware.js: add group membership middleware - backend/src/routes/messageRoute.js: add POST /api/messages/send-group

--- backend/src/routes/messageRoute.js
+++ backend/src/routes/messageRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { sendDirectMessage } from '../controllers/messageController.js';
+import { sendDirectMessage, sendGroupMessage } from '../controllers/messageController.js';
 import { checkFriendship } from '../middlewares/friendMiddleware.js';
-
+import { checkGroupMembership} from '../middlewares/groupMiddleware.js';
 const messageRouter = express.Router();
 
 messageRouter.post('/send-direct', checkFriendship, sendDirectMessage);
+messageRouter.post('/send-group', checkGroupMembership, sendGroupMessage);
 
 export default messageRouter;
