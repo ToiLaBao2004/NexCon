2de724ba3c5cb7d201c52e019e59f94166e425bf
Nguyễn Hoài Bảo <22110108@student.hcmute.edu.vn>
2026-02-13 21:43:05 +0700

    change

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { getCurrentUser, blockUser } from '../controllers/userController.js';
+import { getCurrentUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/me', getCurrentUser);
-userRouter.post('/block/:toUserId', blockUser);
 
 export default userRouter;
